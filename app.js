aac3cd6b0f1be1f48d991f88d9b185915125bfd6
Chris Leichsenring <5677805+cleichsenring@users.noreply.github.com>
2020-08-26 18:59:23 -0400

    Updated error handling for invalid project route

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.use('/static', express.static('public'));
 
 
 app.get('/', (req,res) => {
-  //console.log(projects);
   res.render('index', { projects })
 });
 
@@ -19,27 +18,31 @@ app.get('/about', (req,res) => {
   res.render('about')
 });
 
-app.get('/projects/:id', (req,res) => {
+app.get('/projects/:id', (req,res, next) => {
   const {id} = req.params;
-  const project = projects[id];
-
-  res.render('project', project);
+  //Check to see if the project id is valid. If invalid id, next() to  error 404
+  if (id < projects.length) { 
+    const project = projects[id];
+    res.render('project', project);
+  } else {
+    next();
+  }
 });
 
-
+//404 Not found middleware
 app.use((req,res,next) => {
-  const err = new Error('Uh ohh... The page you went looking for went up in smoke!');
+  const err = new Error('Not Found.  Uh ohh... The page you went looking for went up in smoke!');
   err.status = 404;
   console.log(`Someone tried to navigate to ${req.path} and received a ${err.status} error.`)
   next(err);
 });
 
+//Error handling middleware
 app.use((err,req,res,next) => {
   res.locals.error = err;
   res.status(err.status);
   res.render('error');
 });
-
 
 
 
